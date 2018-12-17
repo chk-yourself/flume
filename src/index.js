1cdfdb2869183c9b46b2970be7aec2554d9d6048
Christina Kim <christinahyunakim@gmail.com>
2018-12-16 20:27:09 -0600

    added vertical and autoplay examples to demo page

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import Flume from './js/flume';
 import './css/styles.css';
 import './css/demo.css';
 
-const slider = new Flume.Slider({
-  id: 'slider1'
+const sliderDefault = new Flume.Slider({
+  id: 'sliderDefault'
+});
+
+const sliderVertical = new Flume.Slider({
+  id: 'sliderVertical',
+  isVertical: true
+});
+
+const sliderAutoplay = new Flume.Slider({
+  id: 'sliderAutoplay',
+  onAutoplay: true
 });
